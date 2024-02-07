c40746a38180c1b2bb89e5e87bfb717c05285bc5
Jonathan Motta <Jonathan.motta44@gmail.com>
2024-02-06 21:03:58 -0300

    Aula 2 Concluida

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,11 @@
 import Logo from './assets/LogoNlwExpert.svg'
 import NoteCard from './components/note-card'
 import { NewNoteCard } from './components/new-note-card'
+
+const note = {
+  date: new Date(),
+  content: 'film ran create giving horn announced think experience underline sold paragraph mighty experiment explanation brick giant without say very struck no shall instrument condition'
+}
 export function App() {
   return (
     <div className='max-w-6xl mx-auto my-12 space-y-6'>
@@ -9,10 +14,10 @@ export function App() {
       <div className='h-px bg-slate-700'></div>
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-     
+        <NoteCard note={note} />
+        <NoteCard note={note}/>
+        <NoteCard note={note}/>
+
 
       </div>
     </div>)
